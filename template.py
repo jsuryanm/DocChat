00197ead319c70e3a5691aa1fddaf655cb75e527
Jayasuryan Mutyala <jsm.dgme@gmail.com>
2026-03-26 11:16:56 +0530

    project directory setup

---
+++ template.py
@@ -0,0 +1,45 @@
+import os 
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level=logging.INFO,
+                    format="[%(asctime)s]: %(message)s")
+
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/agents/__init__.py",
+    "src/agents/relevance_checker.py",
+    "src/agents/workflow.py",
+    "src/agents/research_agent.py",
+    "src/agents/verfication_agent.py",
+    "src/config/__init__.py",
+    "src/config/constants.py",
+    "src/config/settings.py",
+    "src/document_processor/__init__.py",
+    "src/document_processor/file_handler.py",
+    "src/retriever/__init__.py",
+    "src/retriever/builder.py",
+    "src/custom_logger/__init__.py",
+    "src/custom_logger/logger.py",
+    "app.py",
+]
+
+for file_path in list_of_files:
+    file_path =  Path(file_path)
+    file_dir,file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir,exist_ok=True)
+        logging.info(f"Creating directory: {file_dir} for file: {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path,"w") as f:
+            pass
+            logging.info(f"Creating an empty file: {file_path}")
+    
+    else:
+        logging.info(f"{file_name} already exists")
+
+
